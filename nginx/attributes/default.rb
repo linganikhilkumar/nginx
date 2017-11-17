8c41c0c50ca888305aa6705da35c9d4b77e84fc9
root <root@ip-10-0-6-180.us-west-2.compute.internal>
2017-11-17 12:22:30 +0000

    nginx

---
+++ nginx/attributes/default.rb
@@ -0,0 +1,2 @@
+default['nginx']['listen']  = '80'
+default['nginx']['proxypass'] = 'http://34.213.69.148:8080/'
---
+++ nginx/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'nginx'
+
+service 'nginx' do
+	action:start
+end
+template '/etc/nginx/sites-available/default' do
+	source 'default.erb'
+end
